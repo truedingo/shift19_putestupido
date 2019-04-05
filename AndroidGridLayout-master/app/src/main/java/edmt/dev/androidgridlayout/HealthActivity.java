69cff70e1184e7e60dac41bf133c583b7fc81ad6
tiago gomes <tiagogomes@MacBook-Pro-de-Tiago.local>
2019-04-06 00:07:18 +0100

    Actually first commit

---
+++ AndroidGridLayout-master/app/src/main/java/edmt/dev/androidgridlayout/HealthActivity.java
@@ -0,0 +1,80 @@
+package edmt.dev.androidgridlayout;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.Toast;
+
+public class HealthActivity extends AppCompatActivity {
+
+    GridLayout mainGrid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health);
+
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+
+        //Set Event
+        setSingleEvent(mainGrid);
+        //setToggleEvent(mainGrid);
+    }
+
+    private void setToggleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            final CardView cardView = (CardView) mainGrid.getChildAt(i);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
+                        Toast.makeText(HealthActivity.this, "State : True", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                        Toast.makeText(HealthActivity.this, "State : False", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for ( int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent;
+
+                    switch(finalI){
+                        case 0:
+                            intent = new Intent(HealthActivity.this,ActivityOne.class);
+                            intent.putExtra("info","This is activity from card item index boitch "+finalI);
+                            startActivity(intent);
+                            break;
+                        default:
+                            intent = new Intent(HealthActivity.this,ActivityOne.class);
+                            intent.putExtra("info","This is activity from card item index  "+finalI);
+                            startActivity(intent);
+                    }
+
+
+
+                }
+            });
+        }
+    }
+}
